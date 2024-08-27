6341c82d0acaa1c685d7f07e5998cc6bf31949b9
Ross Wilson <ross.wilson@otago.ac.nz>
2024-08-27 11:50:21 +0200

    preliminary docx output format

---
+++ R/as_docx.R
@@ -0,0 +1,9 @@
+#' Export a Typst table or figure object as raw Word XML
+#'
+#' @param x A Typst table or figure (`ttables_tbl` or `ttables_fig`)
+#' @returns A `glue` string containing the raw XML output.
+#'
+#' @export
+as_docx <- function(x) {
+  UseMethod("as_docx", x)
+}
---
+++ R/dxa.R
@@ -0,0 +1,1 @@
+to_dxa <- function(x) as_pt(x) * 20
